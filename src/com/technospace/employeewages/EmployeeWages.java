eaec3f09f951f45425be6930c27f15034da07a14
Janhavi-Khairnar <khairnarj2000@gmail.com>
2023-04-25 10:46:35 +0530

    [ADDED]:[Janhavi] Employee Wages Project with Welcome Message for User

---
+++ src/com/technospace/employeewages/EmployeeWages.java
@@ -0,0 +1,16 @@
+package com.technospace.employeewages;
+
+public class EmployeeWages {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		/*
+		 * Welcome message for user
+		 */
+		System.out.println("Welcome To Employee Wage Computation Program");
+		
+	}
+
+}
