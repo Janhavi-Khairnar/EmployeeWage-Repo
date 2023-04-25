d425ca7ae41d37188755fc24560597d2cbb85337
Janhavi-Khairnar <khairnarj2000@gmail.com>
2023-04-25 16:27:37 +0530

    [MODIFIED]:[Janhavi] Calculating Daily Wages of Employee

--- src/com/technospace/employeewages/EmployeeWages.java
+++ src/com/technospace/employeewages/EmployeeWages.java
@@ -12,11 +12,20 @@ public class EmployeeWages {
 		System.out.println("Welcome To Employee Wage Computation Program");
 		
 		/*
-		 * Using variables for checking the employee is present or
-		 * absent.
+		 * Variables
+		 * attendance : checking employee is present or absent.
+		 * daily_ wages : checking how much money the employee gets daily.
+		 * WAGE_PER_HR : Using to check per hour employee's wage
+		 * FULL_DAY_HR : Full Day duty hour of Employee 
+		 * PART_TIME_HR : Part Day duty hour of Employee 
 		 */
+		
 		int attendance;
+		double daily_wages = 0;
 		final int ABSENT = 0;
+		final int WAGE_PER_HR = 20;
+		final int FULL_DAY_HR = 8;
+		final int PART_TIME_HR = 4;
 		
 		Random random = new Random();
 		attendance = random.nextInt(2);
@@ -24,10 +33,13 @@ public class EmployeeWages {
 		if(attendance == ABSENT)
 		{
 			System.out.println("Employee is absent.");
+			daily_wages = WAGE_PER_HR * PART_TIME_HR;
 		}	else{
 			System.out.println("Employee is present.");
+			daily_wages = WAGE_PER_HR * FULL_DAY_HR;
 		}
 		
+		System.out.println("Daily Wages :$"+daily_wages);
 		
 	}
 
