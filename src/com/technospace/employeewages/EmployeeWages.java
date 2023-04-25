eeee25e4dd2c7017992707b59482293c2a8f4b93
Janhavi-Khairnar <khairnarj2000@gmail.com>
2023-04-25 11:09:20 +0530

    [MODIFIED]:[Janhavi] Check Employee Present Or Absent

--- src/com/technospace/employeewages/EmployeeWages.java
+++ src/com/technospace/employeewages/EmployeeWages.java
@@ -1,15 +1,33 @@
 package com.technospace.employeewages;
 
+import java.util.Random;
+
 public class EmployeeWages {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		
 		/*
 		 * Welcome message for user
 		 */
 		System.out.println("Welcome To Employee Wage Computation Program");
+		
+		/*
+		 * Using variables for checking the employee is present or
+		 * absent.
+		 */
+		int attendance;
+		final int ABSENT = 0;
+		
+		Random random = new Random();
+		attendance = random.nextInt(2);
+		
+		if(attendance == ABSENT)
+		{
+			System.out.println("Employee is absent.");
+		}	else{
+			System.out.println("Employee is present.");
+		}
+		
 		
 	}
 
